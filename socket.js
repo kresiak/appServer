43533c370da22d9f8f26be2c101ad805bc54efdb
AK <kasssalex@gmail.com>
2018-02-26 00:54:09 +0100

    add logging to websocket

--- socket.js
+++ socket.js
@@ -1,12 +1,15 @@
 var webSocket = require('ws');
+const logging = require('./logging')
 
 var wss;
 
 exports.init= (server) => {
     wss = new webSocket.Server({ server: server })
 
-    wss.on('connection', function connection(ws) {
-        
+    wss.on('connection', function connection(ws, req) {
+        const ip = req.connection.remoteAddress;
+        ws.extensions['ip']= ip
+        logging.getLogger().info('web socket connection', ip)
     });    
 }
 
@@ -14,6 +17,7 @@ exports.broadcast= (data) => {
     wss.clients.forEach(function each(client) {
         if (client.readyState === webSocket.OPEN) {
             client.send(JSON.stringify(data));
+            logging.getLogger().info('web socket update to', client.extensions.ip, JSON.stringify(data))
         }
     });
 };
